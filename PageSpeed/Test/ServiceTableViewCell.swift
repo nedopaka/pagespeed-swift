67c7f01411395ad2553c7d735f4c5cd72b6f9a31
Alex <alexander.nedopaka@gmail.com>
2020-02-17 21:03:48 +0200

    3e6SA4OC/6-new-test-page

---
+++ PageSpeed/Test/ServiceTableViewCell.swift
@@ -0,0 +1,55 @@
+//
+//  ServiceTableViewCell.swift
+//  PageSpeed
+//
+//  Created by Alex on 2/15/20.
+//  Copyright © 2020 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class ServiceTableViewCell: UITableViewCell {
+
+    // MARK: - Properties
+
+    var id: String = ""
+
+    var displayName: String {
+        get {
+            nameLabel.text ?? ""
+        }
+        set {
+            nameLabel.text = newValue
+        }
+    }
+
+    var serviceEnabled: Bool {
+        get {
+            serviceSwitch.isOn
+        }
+        set {
+            serviceSwitch.isOn = newValue
+        }
+    }
+
+    // MARK: - IBOutlets
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var serviceSwitch: UISwitch!
+
+    // MARK: - IBActions
+    @IBAction private func serviceSwitchValueChanged(_ sender: Any) {
+
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PageSpeed/Test/TestViewController.swift
@@ -0,0 +1,72 @@
+//
+//  TestViewController.swift
+//  PageSpeed
+//
+//  Created by Alex on 2/15/20.
+//  Copyright © 2020 Stanford University. All rights reserved.
+//
+
+import UIKit
+import Moya
+
+class TestViewController: UIViewController {
+
+    // MARK: - Variables
+    let googleAPIKey = "AIzaSyBD3l3hyQYCJT3_yHCXcD8opyZ1uJer1EA"
+    var provider = MoyaProvider<PageSpeedAPI>()
+
+    // MARK: - IBOutlets
+    @IBOutlet private weak var urlTextField: UITextField!
+    @IBOutlet private weak var responseTextView: UITextView!
+    @IBOutlet private weak var strategySegmentedControl: UISegmentedControl!
+
+    // MARK: - IBAction
+    @IBAction private func performRequest(_ sender: Any) {
+        let url = urlTextField.text!
+        if url.isValidURL {
+            requestToPageSpeed(
+                url: url,
+                strategy: strategySegmentedControl.selectedSegmentIndex == 0 ? "mobile" : "desktop"
+            )
+        }
+    }
+
+    func requestToPageSpeed(
+        url: String,
+        strategy: String,
+        completionHandler: @escaping (_ response: Response?, _ error: MoyaError?) -> Void = { _, _ in }
+    ) {
+        provider.request(.runPagespeed(key: googleAPIKey, url: url, strategy: strategy)) { result in
+            do {
+                let response = try result
+                    .get()
+                    .filter(statusCode: 200)
+                let pageSpeedResult = try response.map(PageSpeedResponse.self)
+                debugPrint(pageSpeedResult)
+                self.responseTextView.text = ""
+                debugPrint(pageSpeedResult, to: &self.responseTextView.text)
+                completionHandler(response, nil)
+            } catch {
+                print(error)
+                completionHandler(nil, error as? MoyaError)
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "New Test"
+        UIView.animate(withDuration: 2, animations: {
+            self.view.backgroundColor = .yellow
+        })
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
